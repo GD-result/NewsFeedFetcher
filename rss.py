09a6b1418c7468926c3d1bd6096538d5487d158b
shoma <mustafin_shamil@yahoo.com>
2012-02-17 20:24:27 +0400

    work, few optimize

--- rss.py
+++ rss.py
@@ -44,41 +44,37 @@ def add_to_base():
     j = len(parsed.entries) - 1;
     content = "";
     NamePage = "";
-    print str(parsed.entries[j].updated)
-    record[1][1]
-    print record[len(record)-1][1]
     while (j >= 0):     
-        i = 0;
-        flag = False;
-        #while (i < len(record)):
+        #i = 0;
+        flag = False
+        ######while (i < len(record)):
         if (parsed.entries[j].updated > record[len(record)-1][1]):
             flag = True
-             #       break;
-           # i += 1;
+        ####     #       break;
+        ####   # i += 1;
         if flag:
             db.commit()  #glyanytb 4tob He picatb ID
-            cur.execute('insert into RSS (id,Date,Title,Author,Link) VALUES (NULL,"%s","%s","%s","%s\
-            ")' % (parsed.entries[j].updated,parsed.entries[j].title,parsed.entries[j].author,parsed.entries[j].link))
+            cur.execute('insert into RSS (id,Date,Title,Author,Link) VALUES (NULL,"%s","%s","%s","%s")' % (parsed.entries[j].updated,parsed.entries[j].title,parsed.entries[j].author,parsed.entries[j].link))
+            
             NamePage = "News Feeds from gg " + str(parsed.entries[j].updated[:10])
-           
-            server = xmlrpclib.ServerProxy('https://wiki.griddynamics.net/rpc/xmlrpc');
-            tokn = server.confluence1.login(WIKI_USER, WIKI_PASS);
+            server = xmlrpclib.ServerProxy('https://wiki.griddynamics.net/rpc/xmlrpc')
+            tokn = server.confluence1.login(WIKI_USER, WIKI_PASS)
             try:
                 page = server.confluence1.getPage(tokn, SPACE, NamePage)
             except:
-                if j == len(parsed.entries) - 1:
-                    k = j
-                else:
-                    k = j + 1
-                NameP = "News Feeds from gg " +str(parsed.entries[k].updated[:10])
-                request(content,NameP)
-                content = ""
+                if j < len(parsed.entries) - 1:
+                    if parsed.entries[j].updated[:10] != parsed.entries[j+1].updated[:10]:
+                            NameP = "News Feeds from gg " + str(parsed.entries[j+1].updated[:10])
+                            request(content,NameP)
+                            content = ""
                 content += "|" + parsed.entries[j].updated + "|" + parsed.entries[j].title + "|" + parsed.entries[j].author +"| \n"
             else:
+                print page['content'].count(str(parsed.entries[j].updated))
                 if page['content'].count(str(parsed.entries[j].updated)) == 0:
                     content += "|" + parsed.entries[j].updated + "|" + parsed.entries[j].title + "|" + parsed.entries[j].author +"| \n"    
             db.commit()
-        j -= 1;
+        j -= 1
     request(content,NamePage);    
     db.close
+
 add_to_base();
