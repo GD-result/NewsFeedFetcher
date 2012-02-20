4ad81a41001760cef1447a6250953672e7ce1124
ileonkina <irinka-leo@yandex.ru>
2012-02-20 16:53:21 +0400

    for pull

--- rss.py
+++ rss.py
@@ -1,4 +1,3 @@
-
 import feedparser
 import sqlite3
 import xmlrpclib
@@ -11,27 +10,36 @@ from conf import token
 from conf import org_name
 from conf import user_name
 
-def request(content,NamePage):
-    server = xmlrpclib.ServerProxy('https://wiki.griddynamics.net/rpc/xmlrpc');
-    token = server.confluence1.login(WIKI_USER, WIKI_PASS);
+#server = xmlrpclib.ServerProxy('https://wiki.griddynamics.net/rpc/xmlrpc');
+#git_token = server.confluence1.login(WIKI_USER, WIKI_PASS);
+
+def request(content, NamePage):
+    #server = xmlrpclib.ServerProxy('https://wiki.griddynamics.net/rpc/xmlrpc');
+    serv = getattr(add_to_base,"server")
+    git_token = serv.confluence1.login(WIKI_USER, WIKI_PASS);
+    tok = git_token
+    print tok, serv
     try:
-        page = server.confluence1.getPage(token, SPACE, NamePage);
+        page = serv.confluence1.getPage(tok, SPACE, NamePage);
     except:
-        parent = server.confluence1.getPage(token, SPACE, TOP_PAGE);
-        table_headers = "h1. News Feed \n ||date||title||author||\n" 
-        page={
+        parent = serv.confluence1.getPage(tok, SPACE, TOP_PAGE);
+        table_headers = "h1. News Feed (UTC) \n ||date||title||author||\n" 
+        page = {
               'parentId': parent['id'],
               'space': SPACE,
               'title': NamePage,
               'content': table_headers + content
               }
-        server.confluence1.storePage(token, page);
+        serv.confluence1.storePage(tok, page);
     else:
         page['content'] += content;
-        server.confluence1.updatePage(token, page,{'versionComment':'','minorEdit':1});
+        serv.confluence1.updatePage(tok, page,{'versionComment':'','minorEdit':1});
 
 
 def add_to_base():
+    add_to_base.server = xmlrpclib.ServerProxy('https://wiki.griddynamics.net/rpc/xmlrpc')
+    add_to_base.git_token = add_to_base.server.confluence1.login(WIKI_USER, WIKI_PASS)
+    print add_to_base.git_token, add_to_base.server
     parsed = feedparser.parse("https://github.com/organizations/" + org_name + "/" + user_name +".private.atom?token=" + token);
     #print parsed.entries[-1];
     db = sqlite3.connect('NewsFeedFetcher.db')
@@ -60,15 +68,15 @@ def add_to_base():
             db.commit()  #glyanytb 4tob He picatb ID
             cur.execute('insert into RSS (id,Date,Title,Author,Link) VALUES (NULL,"%s","%s","%s","%s")' % (parsed.entries[j].updated,parsed.entries[j].title,parsed.entries[j].author,parsed.entries[j].link))
             
-            NamePage = "News Feeds from gg " + str(parsed.entries[j].updated[:10])
-            server = xmlrpclib.ServerProxy('https://wiki.griddynamics.net/rpc/xmlrpc')
-            tokn = server.confluence1.login(WIKI_USER, WIKI_PASS)
+            NamePage = "News Feeds from ggg " + str(parsed.entries[j].updated[:10])
+            #server = xmlrpclib.ServerProxy('https://wiki.griddynamics.net/rpc/xmlrpc')
+            #git_token = server.confluence1.login(WIKI_USER, WIKI_PASS)
             try:
-                page = server.confluence1.getPage(tokn, SPACE, NamePage)
+                page = add_to_base.server.confluence1.getPage(git_token, SPACE, NamePage)
             except:
                 if j < len(parsed.entries) - 1:
-                    if parsed.entries[j].updated[:10] != parsed.entries[j+1].updated[:10]:
-                            NameP = "News Feeds from gg " + str(parsed.entries[j+1].updated[:10])
+                    if parsed.entries[j].updated[:10] != parsed.entries[j + 1].updated[:10]:
+                            NameP = "News Feeds from ggg " + str(parsed.entries[j + 1].updated[:10])
                             request(content,NameP)
                             content = ""
                 content += "|" + parsed.entries[j].updated + "|" + parsed.entries[j].title + "|" + parsed.entries[j].author +"| \n"
@@ -80,5 +88,4 @@ def add_to_base():
         j -= 1
     request(content,NamePage);    
     db.close
-
-add_to_base();
+add_to_base()
