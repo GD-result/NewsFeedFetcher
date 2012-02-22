a8ba408aa3ce4a2753694c50c7e0db319098dab2
shoma <mustafin_shamil@yahoo.com>
2012-02-22 17:23:49 +0400

    pull

--- rss.py
+++ rss.py
@@ -31,12 +31,14 @@ def request(content, NamePage):
         page['content'] += content
         server.confluence1.updatePage(wiki_token, page, {'versionComment': '', \
                                                          'minorEdit': 1})
-        
+def bbb():
+    return add_to_base.cur    
 
 def update(parsed,j,id_n):
     add_to_base.cur.execute("""insert into RSS (id,Date,Title,Author,Link) VALUES \
     (NULL,"%s","%s","%s","%s")""" % (parsed.entries[j].updated, parsed.entries[j].title, parsed.entries[j].author, parsed.entries[j].link))                           
     add_to_base.content += "|" + str(id_n) + "|" + parsed.entries[j].updated + "|" + parsed.entries[j].title + "|" + parsed.entries[j].author + "| \n"
+    return add_to_base.content
 
 
 def add_to_base():
@@ -46,7 +48,7 @@ def add_to_base():
     add_to_base.cur = db.cursor()
     add_to_base.cur.execute("""create table if not exists RSS (id INTEGER PRIMARY KEY 
     AUTOINCREMENT,Date,Title,Author,Link)""")
-    j = len(parsed.entries) - 1
+    j = len(parsed.entries) - 25
     add_to_base.content = ""
     NamePage = ""
     one = True
@@ -64,7 +66,7 @@ def add_to_base():
             flag = True
             id_n = 1
         if flag:          
-            NamePage = "News Feeds from gith " + str(parsed.entries[j].updated[:10])
+            NamePage = "News Feeds from github1233333 " + str(parsed.entries[j].updated[:10])
             try:
                 if (parsed.entries[j].updated[:10] != parsed.entries[j - 1].updated[:10]) or (one):
                     page = server.confluence1.getPage(wiki_token, SPACE, NamePage)
@@ -78,8 +80,10 @@ def add_to_base():
             else:
                 if not new:                   
                     update(parsed,j,id_n)
+                    one = False
                 else:    
                     new = False
+                    one = False
                     if page['content'].count(str(parsed.entries[j].updated)) == 0:
                         update(parsed,j,id_n)
             db.commit()
@@ -87,3 +91,27 @@ def add_to_base():
     request(add_to_base.content, NamePage)
     db.close
 add_to_base()
+
+db = sqlite3.connect('NewsFeedFetcher.db')    
+
+c = db.cursor()
+
+c.execute('SELECT * FROM RSS')
+
+record = c.fetchall()
+
+c.execute('SELECT * FROM RSS')
+
+for row in c:
+
+    print row
+
+print
+parsed = feedparser.parse("https://github.com/organizations/"\
+ + org_name + "/" + user_name + ".private.atom?token=" + token)
+i = 1
+for e in parsed.entries:
+    print i,e
+    i +=1
+
+print record[len(record)-1][0]
