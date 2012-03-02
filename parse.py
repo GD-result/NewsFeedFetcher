bc4dcdd8dac7f32169b23142a89963e7c6c9404a
ileonkina <irinka-leo@yandex.ru>
2012-03-02 18:00:08 +0400

    it works!!!

--- parse.py
+++ parse.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import sqlite3
+import base64
 
 from conf import github_login
 from conf import github_pass
@@ -37,27 +38,20 @@ def add_news():
         last_date_from_db = ""
     else:
         last_date_from_db = record[0][1]
-    #print last_date_from_db, count
     count = 0
     #print news[0]
     while (count < len(news) and news[count]['created_at'] > last_date_from_db):
         count += 1
         #print "ololo"
     count -= 1
-    #summary = ""
-    #summary = json.dumps("iplllllllllllllllllllllllllllllllllllllllllllllm")
-    #cur.execute('insert into RSS (id, Date, Author, EventType, Summary) VALUES (NULL,"%s","%s","%s","%s")'\
-    #    % (news[count]['created_at'], news[count]['actor']['login'], news[count]['type'], summary))
     while (count >= 0):
         summary = json.dumps(news[count])
+        #print news[count]
         cur.execute('insert into RSS (id, Date, Author, EventType, Summary) VALUES (NULL,"%s","%s","%s","%s")'\
-        % (news[count]['created_at'], news[count]['actor']['login'], news[count]['type'], summary))
-        db.commit()   
+        % (news[count]['created_at'], news[count]['actor']['login'], news[count]['type'], base64.b64encode(summary)))
+        db.commit()  
         count -= 1
-    db.close
-    #print "iiiiii"
-    #print summary
-    #print type(json.dumps("shgg")) 
+    db.close 
 
     
 def print_base():
@@ -65,16 +59,19 @@ def print_base():
     print base()
     
     Use this function to print all data base in console
+    Test function
     """  
     db = sqlite3.connect('NewsFeed.db')
     cur = db.cursor()
     cur.execute('create table if not exists RSS (id INTEGER PRIMARY KEY AUTOINCREMENT,Date,Title,Author,Link,Summary)')
-    cur.execute('SELECT max(id), Summary FROM RSS')
+    cur.execute('SELECT * FROM RSS')
     db.commit()
     db.close
-    rec = cur.fetchall
-    n = json.loads(rec[0][1])
-    print n
+    rec = cur.fetchall()
+    for i in range(len(rec)):
+        n = base64.b64decode(rec[i][4])
+        print rec[i][0], rec[i][1], rec[i][2], rec[i][3], n
+        print json.loads(n)
 #    for elem in cur:
 #        print elem
         
